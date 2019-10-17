09b4e09811a8e9bba5a16820c8cc6bc280fcbecc
vicctoti <victor_h_fernandez@estudante.sc.senai.br>
2019-10-17 16:55:36 -0300

    Atualização do CART
    Rota do cart finalizada, redirecionando para o carrinho.
    
    -Terminar o calculo final
    -Implementar a quantidade.
    -Tirar a redireção do botão adicionar

--- src/routes/cartRoutes.php
+++ src/routes/cartRoutes.php
@@ -7,14 +7,29 @@ use Slim\Http\Response;
 return function  (App $app) {
     $container = $app->getContainer();
 
-    $app->get('/cart/', function (Request $request, Response $response, array $args) use ($container) {
+    $app->get('/cart/[{idproduto}]', function (Request $request, Response $response, array $args) use ($container) {
+        // Sample log message
+        $container->get('logger')->info("Slim-Skeleton '/' route");
+        $conexao = $container->get('pdo');
+
+        $resultSet = $conexao->query('SELECT * from produto where idProduto = '. $args['idproduto'].'')->fetchAll();
+        
+        $_SESSION['produtos'][] = $resultSet;
+
+        
+   
+        
+        // Render index view
+        return $container->get('renderer')->render($response, 'cart.phtml', $args);
+    });
+
+    $app->get('/cartlimpar/', function (Request $request, Response $response, array $args) use ($container) {
         // Sample log message
         $container->get('logger')->info("Slim-Skeleton '/' route");
 
-        
-
+        session_destroy();
 
         // Render index view
-        return $container->get('renderer')->render($response, 'cart.phtml', $args);
+        return $container->get('renderer')->render($response, 'index.phtml', $args);
     });
 };
